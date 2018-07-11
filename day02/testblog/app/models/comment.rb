97dc68e985c8968a0bdad902eb9b5d55b95ca658
Sebastian Andrzejczak <214877@edu.p.lodz.pl>
2018-07-11 10:35:30 +0200

    Validation for comment.

--- day02/testblog/app/models/comment.rb
+++ day02/testblog/app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
-
-  
+  validates :author_id, :post_id, :content, presence: true
+  validates :content, uniqueness: true
+  validates :content, length: { minimum: 5 }
 end
--- day02/testblog/db/schema.rb
+++ day02/testblog/db/schema.rb
@@ -30,9 +30,11 @@ ActiveRecord::Schema.define(version: 2018_07_10_134836) do
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "author_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
